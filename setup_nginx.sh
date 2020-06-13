b5e1aa4be58b4e8b6652dc451ed5bbb144cbac98
satya0307-d <satya0307@yahoo.co.in>
2020-06-13 13:31:22 +0530

    Create setup_nginx.sh

---
+++ setup_nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+wget https://nginx.org/download/nginx-1.18.0.tar.gz
+tar xzf nginx-1.18.0.tar.gz
+apt-get install git -y
+git clone https://github.com/satya0307-d/nginx-rtmp-module.git
+apt-get install libpcre3 libpcre3-dev -y
+apt-get install build-essential -y
+apt-get install libssl-dev -y
+apt-get install zlib1g-dev -y
+cd nginx-1.18.0
+./configure --add-module=/root/nginx-rtmp-module
+make
+make install
