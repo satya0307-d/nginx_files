6b7319605804440f826f57703ceab0ab903e8a7c
satya0307-d <satya0307@yahoo.co.in>
2020-06-13 13:30:09 +0000

    Update connect changes for taking ip address as input

--- commands/connect_ssh.py
+++ commands/connect_ssh.py
@@ -2,8 +2,12 @@ import os
 import paramiko
 import keyring
 import subprocess
+import sys, getopt
 
-subprocess.run(["scp", "-i", "/root/secret/mykey.pem", "/root/nginx_files/setup_nginx.sh", "root@165.22.209.207:/root/."])
+
+param_1= sys.argv[1] 
+print(param_1)
+subprocess.run(["scp", "-i", "/root/secret/mykey.pem", "/root/nginx_files/setup_nginx.sh", "root@"+param_1+":/root/."])
 
 keyfile = os.path.expanduser('/root/secret/mykey.pem')
 key = paramiko.RSAKey.from_private_key_file(keyfile)
@@ -13,14 +17,19 @@ ssh = paramiko.SSHClient()
 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print("connecting")
-ssh.connect('165.22.209.207', username='root', password = '', pkey = key )
+ssh.connect(param_1, username='root', password = '', pkey = key )
 ssh.invoke_shell()
 print("connected")
 
 stdin, stdout, stderr = ssh.exec_command('ls')
 print (stdout.read())
-#print ('SCP connecting')
-#stdin, stdout, stderr = ssh.exec_command('sudo scp -i /root/secret/mykey.pem /root/nginx_files/setup_nginx.sh root@165.22.209.207:/root/.')
+print ('Change chmod')
+stdin, stdout, stderr = ssh.exec_command('chmod 777 /root/./setup_nginx.sh')
+print (stdout.read())
+print (stderr.read())
+#print ('Executing shell script')
+#stdin, stdout, stderr = ssh.exec_command('/root/./setup_nginx.sh')
 #print (stdout.read())
 #print (stderr.read())
 ssh.close()
+
